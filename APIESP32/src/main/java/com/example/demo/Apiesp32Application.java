b5e4e4b51a4ae2857333f9a5f63e4cfaa3069a20
ElTrompa <159024200+Trompa06@users.noreply.github.com>
2026-01-08 13:29:38 +0100

    estructuraAPI

---
+++ APIESP32/src/main/java/com/example/demo/Apiesp32Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Apiesp32Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Apiesp32Application.class, args);
+	}
+
+}
